05442ddcf5d2f55b2fddcdfb83e11476f5e375d3
Sourish Mallick <squaareart27@gmail.com>
2025-08-16 12:43:04 +0530

    Login form created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,23 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css'
-import Login from './components/auth/Login'
-import Signup from './components/auth/Signup'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Signup from './components/auth/Signup';
+import Login from './components/auth/Login';
+
 
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Job Portal</h1>
-        <h1>Login</h1>
-        <h1>Signup</h1>
-        <p>Welcome to the Job Portal Application</p>
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test HMR
-          </p>
-        </div>
-        <p className="read-the-docs">
-          Click on the Job Portal logo to learn more
-        </p>
-      </header>
+      <Router>
+          <Routes>
+            <Route path="/signup" element={<Signup />} />
+             <Route path="/login" element={<Login />} />
+          </Routes>
+      </Router>
+      <h1>Welcome to the App</h1>
     </div>
   )
 }
