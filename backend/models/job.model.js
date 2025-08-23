f4028ed30d6dbd3aee57ae44a6019726fa8c3cac
Arashi <dsoma1099@gmail.com>
2025-08-23 20:26:03 +0530

    balls

--- backend/models/job.model.js
+++ backend/models/job.model.js
@@ -1,21 +1,27 @@
-import mongoose from 'mongoose';
-
+import mongoose from "mongoose";
 
 const jobSchema = new mongoose.Schema(
-{
-title: { type: String, required: true, trim: true },
-company: { type: String, required: true, trim: true },
-location: { type: String, required: true, trim: true },
-type: { type: String, enum: ['full-time', 'part-time', 'contract', 'internship'], default: 'full-time' },
-salaryMin: { type: Number },
-image: { type: String },
-salaryMax: { type: Number },
-description: { type: String, required: true },
-requirements: [{ type: String }],
-createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
-},
-{ timestamps: true }
+  {
+    title: { type: String, required: true, trim: true },
+    company: { type: String, required: true, trim: true },
+    location: { type: String, required: true, trim: true },
+    type: {
+      type: String,
+      enum: ["full-time", "part-time", "contract", "internship"],
+      default: "full-time",
+    },
+    salaryMin: { type: Number },
+    image: { type: String },
+    salaryMax: { type: Number },
+    description: { type: String, required: true },
+    requirements: [{ type: String }],
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
 );
 
-
-export default mongoose.model('Job', jobSchema);
+export default mongoose.model("Job", jobSchema);
--- frontend/src/components/card.jsx
+++ frontend/src/components/card.jsx
@@ -1,19 +1,17 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Card} from 'react-bootstrap';
-import {Button} from 'react-bootstrap';
+import {Card, Button} from 'react-bootstrap';
 import './card.css';
-const JobCard = () => {
+const JobCard = ({job}) => {
   return (
     <>
     <div>
     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Img variant="top" src={`${job?.image}`}/>
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>{job?.title}</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+          {job?.description}
         </Card.Text>
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
