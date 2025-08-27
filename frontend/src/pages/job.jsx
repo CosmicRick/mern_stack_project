1c3dcb25f6155d44da00c1a3024c752ac8f1f3e4
Arashi <dsoma1099@gmail.com>
2025-08-27 15:15:08 +0530

    dummy apply button

--- frontend/src/pages/job.jsx
+++ frontend/src/pages/job.jsx
@@ -66,6 +66,8 @@ const Jobs = () => {
     const diffTime = Math.abs(now - posted);
     return Math.ceil(diffTime / (1000 * 60 * 60 * 24));
   };
+// Applied state
+const [applied, setApplied] = useState(false);
 
   return (
      < >
@@ -197,7 +199,16 @@ const Jobs = () => {
                         </div>
                       </div>
                       <div>
-                       
+                         <button
+    onClick={() => setApplied((prev) => !prev)}
+    className={`px-4 py-2 rounded-lg font-semibold transition-all shadow-md ${
+      applied
+        ? "bg-green-500 text-white hover:bg-green-600"
+        : "bg-gradient-to-r from-green-600 to-green-800 text-black hover:from-green-800 hover:to-green-900"
+    }`}
+  >
+    {applied ? "Applied" : "Apply Now"}
+  </button>
                           <Link
     to={`/jobs/${job._id}`}
     className="bg-gradient-to-r from-green-600 to-green-800 text-black px-4 py-2 rounded-lg font-semibold hover:from-green-800 hover:to-green-900 transition-all shadow-md ml-3 inline-block"
